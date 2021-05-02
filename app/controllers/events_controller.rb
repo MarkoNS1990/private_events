4923854f2eb89379e33d3ac6b5eed745abdd439a
Marko Zecevic <marczecevic@gmail.com>
2021-05-02 16:31:26 +0200

    Fix event cards layout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -27,7 +27,8 @@ class EventsController < ApplicationController
 
   # POST /events or /events.json
   def create
-    @event = Event.new(event_params)
+    @user = current_user
+    @event = @user.events.build(event_params)
 
     respond_to do |format|
       if @event.save
