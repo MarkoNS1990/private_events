146ee1afc4bc8ce690510369319bf2e5fec85f15
Marko Zecevic <marczecevic@gmail.com>
2021-05-02 18:18:11 +0200

    Add rspec to project and tests for creating an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,11 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :attendees
 
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :title, presence: true
+  validates :date, presence: true
+
   scope :upcoming_events, ->(time = Time.now.midnight) { where('date > ?', time) }
   scope :past_events, ->(time = Time.now.midnight) { where('date < ?', time) }
 end
